dbf00b2d5363c3c402d2ded95f644d8dc49662b7
v <victusves@gmail.com>
2025-08-03 13:50:37 +0330

    test

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class homepage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 26, 27, 28),
       appBar: AppBar(
-        elevation: 0,
+        elevation: 1,
         backgroundColor: const Color.fromARGB(113, 0, 0, 0),
         actions: [
           CircleAvatar(
