84a818733b3fe3371ab2f9c541ba7fef6cd243b8
Aref Moosavi <Aref.Moosavi7813@gmail.com>
2025-08-06 10:14:05 +0330

    add refreshbutton ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -40,6 +41,7 @@ class CurrencyV extends StatelessWidget {
   }
 }
 
+//----------------------------------------------------
 class homepage extends StatelessWidget {
   const homepage({super.key});
 
@@ -65,75 +67,126 @@ class homepage extends StatelessWidget {
           SizedBox(width: 10),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(Icons.currency_exchange, color: Colors.white),
-                SizedBox(width: 10),
-                Text(
-                  'نرم افزاری برای مشاهده نرخ ارز و کریپتوکارنسی',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Container(
-              width: double.infinity,
-              height: 35,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(146, 224, 109, 101),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text('نام', style: Theme.of(context).textTheme.headlineLarge),
+                  Icon(Icons.currency_exchange, color: Colors.white),
+                  SizedBox(width: 10),
                   Text(
-                    'قیمت',
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  Text(
-                    'تغییرات',
-                    style: Theme.of(context).textTheme.headlineLarge,
+                    'نرم افزاری برای مشاهده نرخ ارز و کریپتوکارنسی',
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 15),
-            SizedBox(
-              width: double.infinity,
-              height: 350,
-              child: ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: 10,
-                itemBuilder: (BuildContext context, int positsion) {
-                  return Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(blurRadius: 1.0, color: Colors.grey),
-                        ],
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-
-                      width: double.infinity,
-                      height: 50,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [Text('دلار'), Text('93700'), Text('+43')],
-                      ),
+              SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                height: 35,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(146, 224, 109, 101),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'نام',
+                      style: Theme.of(context).textTheme.headlineLarge,
                     ),
-                  );
-                },
+                    Text(
+                      'قیمت',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    Text(
+                      'تغییرات',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                  ],
+                ),
               ),
-            ),
+              SizedBox(height: 15),
+              SizedBox(
+                width: double.infinity,
+                height: 500,
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Items();
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                child: Container(
+                  width: double.infinity,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(1000),
+                    color: const Color.fromARGB(146, 224, 109, 101),
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: double.infinity,
+                        child: TextButton.icon(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(
+                              Colors.blueGrey,
+                            ),
+                          ),
+                          onPressed: () {},
+                          label: Text(
+                            'بروزرسانی',
+                            style: Theme.of(context).textTheme.headlineLarge,
+                          ),
+                          icon: Icon(
+                            CupertinoIcons.refresh,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//----------------------------------------------------
+
+class Items extends StatelessWidget {
+  const Items({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: <BoxShadow>[
+            BoxShadow(blurRadius: 1.0, color: Colors.grey),
           ],
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(15),
+        ),
+
+        width: double.infinity,
+        height: 55,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [Text('دلار'), Text('93700'), Text('+43')],
         ),
       ),
     );
